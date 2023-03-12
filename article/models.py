c9af74639d1bbeb394f81307e0add610ac3a12ca
Dilnaz <dilnaz.yussupova@narxoz.kz>
2023-03-12 22:58:52 +0600

    lab 6

--- article/models.py
+++ article/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d")
+    time_create = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
 def __str__(self):
     return self.title
+
 class Styles(models.Model):
     style_name = models.CharField(max_length=255, db_index=True)
-
 
 class Users(models.Model):
     name = models.CharField(max_length=50, db_index=True)
